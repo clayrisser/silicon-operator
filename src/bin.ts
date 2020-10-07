3ffff485dd467bd6285ddcc905226e67bbf3a1c5
Jam Risser <jam@codejam.ninja>
2020-10-07 23:59:58 +0000

    Use velero schedule operator

--- src/bin.ts
+++ src/bin.ts
@@ -1,8 +1,8 @@
-import Velero from './velero';
+import { VeleroOperator } from '@operators/velero-schedule-replicator';
 import config from './config';
 
 (async () => {
-  const velero = new Velero(config);
+  const velero = new VeleroOperator(config);
   await velero.start();
 
   function exit(_reason: string) {
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export { default as Config } from './config';
 export { default as Logger } from './logger';
-export { default as VeleroOperator } from './velero';
 export * from './config';
 export * from './logger';
 export * from './types';
-export * from './velero';
