382d4f7d8b564ac67e44f0b077f63a304dc3932e
Jam Risser <jam@codejam.ninja>
2020-10-08 07:32:38 +0000

    Added helm2cattle operator

--- src/bin.ts
+++ src/bin.ts
@@ -1,12 +1,15 @@
+import { Helm2CattleOperator } from '@operators/helm2cattle';
 import { VeleroOperator } from '@operators/velero-schedule-replicator';
 import config from './config';
 
 (async () => {
-  const velero = new VeleroOperator(config);
-  await velero.start();
+  const helm2CattleOperator = new Helm2CattleOperator(config);
+  const veleroOperator = new VeleroOperator(config);
+  await Promise.all([helm2CattleOperator.start(), veleroOperator.start()]);
 
   function exit(_reason: string) {
-    velero.stop();
+    helm2CattleOperator.stop();
+    veleroOperator.stop();
     process.exit(0);
   }
 
